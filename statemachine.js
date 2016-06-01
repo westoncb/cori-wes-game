eb1e3e75d27e34d0552cf823f7e370a0b869b2c9
Weston C. Beecroft <westoncb@gmail.com>
2016-06-01 15:52:39 -0600

    A few small fixes to statemachine.js
    - Set ‘currentState’ when transitioning
    - Add ‘return’ statement to stateWithName
    - Make ‘connectStatePair’ take state names rather than states

--- statemachine.js
+++ statemachine.js
@@ -34,12 +34,14 @@ class StateMachine {
     //Edge might be undefined if we're transitioning for the first time,
     //and we only want to print this when that's not the case.
     else if (currentState) {
-      console.log("Edge not found; could not transition.");
+      console.log("Edge to'" + nextStateName + "' not found; could not transition.");
     }
 
     //We check for '!currentState' since on the first transition 'currentState'
     //might be undefined but we want to cal its 'enteringStateFunc' anyway.
     if (!currentState || edge) {
+      this.currentState = nextState;
+
       var enteringStateFunc = nextState.enteringStateFunc;
       if (enteringStateFunc) {
         enteringStateFunc();
@@ -48,17 +50,23 @@ class StateMachine {
   }
 
   edgeForStatePair(startState, destState) {
+
+    if (!startState) return null;
+
     return startState.outgoingEdges.find(edge => edge.dest.name === destState.name);
   }
 
   stateWithName(name) {
-    this.states.find(state => {return state.name === name;});
+    return this.states.find(state => {return state.name === name;});
   }
 
-  connectStatePair(first, second) {
-    var edge = {start: first, dest: second};
+  connectStatePair(startStateName, destStateName) {
+    var startState = this.stateWithName(startStateName);
+    var destState = this.stateWithName(destStateName);
 
-    first.outgoingEdges.push(edge);
+    var edge = {start: startStateName, dest: destState};
+
+    startState.outgoingEdges.push(edge);
   }
 
   start() {
